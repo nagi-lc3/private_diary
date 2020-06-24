e0620263dd9ee621948527b18618c8cab657302a
nagi <ymxlinkinparkxlc@gmail.com>
2020-06-24 20:08:03 +0900

    Fix settings_common.py

--- private_diary/settings_common.py
+++ private_diary/settings_common.py
@@ -151,7 +151,6 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',  # 管理サイト用（ユーザー名認証）
 )
 
-LOGIN_REDIRECT_URL = 'diary:diary_list'
 
 # メールアドレス認証に変更する設定
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
@@ -162,7 +161,7 @@ ACCOUNT_EMAIL_VERIFICATION = 'mandatory'
 ACCOUNT_EMAIL_REQUIRED = True
 
 # ログイン／ログアウト後の遷移先設定
-LOGIN_REDIRECT_URL = 'diary:index'
+LOGIN_REDIRECT_URL = 'diary:diary_list'
 ACCOUNT_LOGOUT_REDIRECT_URL = 'account_login'
 
 # ログアウトリンクのクリック一発でログアウトする設定
